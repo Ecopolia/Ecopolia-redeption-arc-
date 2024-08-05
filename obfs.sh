796bde4b1325660940efb3a8a9006521967e3bfd
Plep-m <menut_p@etna-alternance.net>
2024-08-05 12:37:28 +0200

    Feature: obfuscate src code

--- obfs.sh
+++ obfs.sh
@@ -3,3 +3,8 @@ for file in $(find . -path ./prometheus -prune -o -iname "*.lua" -print) ; do
     echo "obfuscating $file"
     lua ./prometheus/cli.lua --preset Strong $file
 done
+
+for file in $(find . -iname "*.obfuscated.lua" -print) ; do
+    echo "renaming $file to ${file%.obfuscated.lua}.lua"
+    mv $file ${file%.obfuscated.lua}.lua
+done
