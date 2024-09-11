e63898ce488df7be3823c638434cf0c9325a7ec7
pmenut <p.menut@paritel.fr>
2024-09-11 17:27:08 +0200

    Feature: improving ATB with bubbles and outlines

--- objects/npc.lua
+++ objects/npc.lua
@@ -1,12 +1,13 @@
 NPC = {}
 NPC.__index = NPC
 
-function NPC.new(name, color, speed)
+function NPC.new(name, color, speed, group)
     local self = setmetatable({}, NPC)
     self.name = name or "NPC"
     self.color = color or {1, 0, 0}
     self.speed = speed or 1
     self.progress = 0
+    self.group = group or 'ally'
     return self
 end
 
